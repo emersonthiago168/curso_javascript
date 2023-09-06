d3c50ef5a1e15eb63b1fb7fb2b4d730a4ba1b550
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-06 20:41:03 -0300

    condição aplicada para cada idade

--- exercicios/exercicio2/script.js
+++ exercicios/exercicio2/script.js
@@ -10,16 +10,33 @@ function verificar () {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+            } else if (idade < 21) {
+                //jovem
+            } else if (idade < 50) {
+                //adulto
+            } else {
+                //idoso
+            }
         }
-        res.innerHTML = `Detectamos ${genero} com ${idade}`
-        
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
     } 
-
-    
-
 }
