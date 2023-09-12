1d677b2e3c61bae960d366651de681efea17cea2
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-12 12:12:23 -0300

    Update vetor-na-tela.js

--- aula15/vetor-na-tela.js
+++ aula15/vetor-na-tela.js
@@ -1,6 +1,8 @@
 let valores = [8, 1, 7, 4, 2, 9]
 valores.sort()
 
-for (e = 0; e < valores.length; e++) {
-    console.log(valores[e])
+console.log(valores)
+
+for (e=0; e < valores.length; e++) {
+    console.log(`A posição ${e} tem o valor ${valores[e]}`)
 }
