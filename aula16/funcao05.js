6bc0fa2fea25e49c125fba82750b7d168b12dab9
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-12 16:01:59 -0300

    Update funcao05.js

--- aula16/funcao05.js
+++ aula16/funcao05.js
@@ -1,6 +1,13 @@
+// RECURSIVIDADE
 function fatorial(n) {
-    if ()
+    if (n == 1 || n == 0) {
+        return 1
+    } else {
+        return n * fatorial(n-1)
+    }
 }
+
+console.log(fatorial(5))
 
 
 
