3d9213dd7897698588abc0d87e01993a81d8073f
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-12 20:48:58 -0300

    atualizoado

--- exercicios/exercicio5/script.js
+++ exercicios/exercicio5/script.js
@@ -1,17 +1,9 @@
 let num = document.getElementById('fnum')
-let lista = document.getElementById('lista')
+let lista = document.getElementById('flista')
 let res = document.getElementById('res')
 let valores = []
 
-function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
-        
-    } else {
-        window.alert('Valor inválido ou já encontrado na lista.')
-    }
-}
-
-function isNumero (n) {
+function isNumero(n) {
     if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
@@ -26,3 +18,34 @@ function inLista(n, l) {
         return false
     }
 }
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+
+    }
+}
+
+
+
+
