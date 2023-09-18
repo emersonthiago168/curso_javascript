4e40e0d773087003c80c5b7ba00e22a57039c03b
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-18 16:11:25 -0300

    alert, confirm e prompt (navegador)

---
+++ curso da udemy/aula11/js/script.js
@@ -0,0 +1,5 @@
+let num1 = window.prompt('Digite um número');
+let num2 = window.prompt('Digite outro número');
+let resultado = Number(num1) + Number(num2);
+
+window.alert(`A soma entre os números ${num1} e ${num2} é:  ${resultado}`)
