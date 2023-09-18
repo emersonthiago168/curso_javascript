e09c617c1be4f84395b9e47349ecc5ce1b1d6731
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-18 14:58:45 -0300

    Constantes com const

---
+++ curso da udemy/aula7/index.js
@@ -0,0 +1,11 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos 
+// Não podemos começar o nome de uma constante com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos modificar o valor de uma constante 
+// NÂO UTILIZE VAR, UTILIZE CONST.
+
+const nome = 'João';
+console.log(nome);
