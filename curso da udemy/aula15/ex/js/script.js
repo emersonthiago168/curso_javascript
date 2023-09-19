1e20fea4683f3a38f243231207706f02662c315c
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-19 16:22:21 -0300

    exercicio com numbers e math

--- curso da udemy/aula15/ex/js/script.js
+++ curso da udemy/aula15/ex/js/script.js
@@ -5,11 +5,11 @@ let num = Number(numtxt)
 
 titulo.innerHTML += `${num}`
 resultado.innerHTML = `
-Raiz quadrada: ${num ** 0.5} <br />
-${num} é inteiro: ${Number.isInteger(num)} <br />
-É NaN: ${Number.isNaN(num)} <br />
-Arredondando para baixo: ${Math.floor(num)} <br />
-Arredondando para cima: ${Math.ceil(num)} <br />
-Arredondando para o mais próximo: ${Math.round(num)} <br />
-Com duas casas decimais: ${num.toFixed(2)}
+    Raiz quadrada: ${num ** 0.5} <br />
+    ${num} é inteiro: ${Number.isInteger(num)} <br />
+    É NaN: ${Number.isNaN(num)} <br />
+    Arredondando para baixo: ${Math.floor(num)} <br />
+    Arredondando para cima: ${Math.ceil(num)} <br />
+    Arredondando para o mais próximo: ${Math.round(num)} <br />
+    Com duas casas decimais: ${num.toFixed(2)}
 `
