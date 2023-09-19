ccbd5d5d34a938aed18f44a0a8ecee4a0adfaf5e
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-19 16:03:25 -0300

    objeto math

---
+++ curso da udemy/aula15/index.js
@@ -0,0 +1,12 @@
+console.log(!!(100 / 0));
+
+// console.log(100 / 0); valor = infinity true
+// let num2 = Math.floor(num1); arredonda para baixo
+// let num2 = Math.ceil(num1); arredonda para cima
+// let num2 = Math.round(num1); arredonda para o mais próximo
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)) pega o maior número
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)) pega o menor número
+
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5); gera um número aleário entre 10 e 5 e arredonda para o mais próximo
+// console.log(aleatorio);
+
