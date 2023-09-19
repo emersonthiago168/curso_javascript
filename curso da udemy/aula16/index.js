1bac997cb2a1e6e847ee945d6a337bee2457ae66
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-19 17:21:14 -0300

    array (básico)

---
+++ curso da udemy/aula16/index.js
@@ -0,0 +1,42 @@
+
+//            0123
+const nome = 'João'
+
+//                0        1        2         3      4
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+// alunos.push('Luiza');
+// alunos.push('Eduardo');
+// console.log(alunos.slice(0, -2))
+
+// console.log(alunos[50]);
+
+// delete alunos[1]; deleta o índice indicado mas não altera os outros
+// console.log(alunos[1]);
+
+// const removido = alunos.shift(); // removido no começo
+// console.log(removido)
+// console.log(alunos)
+
+// const removido = alunos.pop(); // remove do fim
+// console.log(removido)
+
+// alunos.unshift('Luiza') // Adiciona no começo e os outros avançam um índice
+// alunos.unshift('Fábio') 
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Otávio');
+
+// alunos[alunos.length] = 'Luiza' // Adiciona no fim
+// alunos[alunos.length] = 'Fábio'
+// alunos[alunos.length] = 'Luana'
+
+// alunos[0] = 'Eduardo' // Altera
+// alunos[3] = 'Luiza' // Adicionando
+
+// console.log(alunos)
+// console.log(alunos[0])
+// console.log(alunos[2])
