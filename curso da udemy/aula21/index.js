5f76fd4637c98bda9df1ffa1d1fd3db3e395b2c8
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-20 10:28:10 -0300

    operadores de comparação

---
+++ curso da udemy/aula21/index.js
@@ -0,0 +1,12 @@
+/* 
+Operadores de comparação
+> maior que
+>= maior ou igual a 
+< menor que
+<= menor que ou igual a
+== igualdade (valor) **********
+=== igual estrita (valor e tipo)
+!= diferente (valor) ***********
+!== diferente estrito (valor e tipo)
+*/
+
