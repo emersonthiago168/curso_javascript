6be8b5cc2bb7863a73a4fdb18564cd91ce190500
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-25 17:32:19 -0300

    switch/case

---
+++ curso da udemy/aula29/index.js
@@ -0,0 +1,37 @@
+function getdiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            break;
+        case 6:
+            diaSemanaTexto = 'Sábado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = ''
+            return diaSemanaTexto;
+    };
+}
+
+const data = new Date('1987-04-25 00:00:00');
+const diaSemana = data.getDay();
+const diaSemanaTexto = getdiaSemanaTexto(diaSemana);
+
+
+console.log(diaSemana, diaSemanaTexto);
