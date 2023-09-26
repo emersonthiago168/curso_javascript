12b7f81f3b99fdcc58bd3d2df8add7ea4b13572b
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-26 16:24:46 -0300

    atribuição via estruturação

---
+++ curso da udemy/aula32/index.js
@@ -0,0 +1,10 @@
+// ... rest, ... spread
+// const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+// const [um, dois, tres] = numeros;
+// console.log(um, dois, tres)
+
+
+const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9]  ];
+const [lista1, lista2, lista3] = numeros;
+console.log(lista3[2])
+
