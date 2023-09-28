aea3826fbd076ab5ba0175383ac2e89ecd84cdef
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-28 19:57:07 -0300

    tratando e lançando erros (try, catch, finally)

---
+++ curso da udemy/aula45/index.js
@@ -0,0 +1,23 @@
+function retornaHora(data = new Date()) {
+    if (data && !(data instanceof Date)) {
+        throw new TypeError('Esperando instância de Date')
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    })
+}
+
+try {
+    const data = new Date('01-01-1970 12:58:12');
+    const hora = retornaHora(50);
+    console.log(hora)
+} catch (e) {
+    // Tratar erro
+} finally {
+    console.log('Tenha um bom dia')
+}
+
