43715dc89e472f1504613ea67b15b171143779f9
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-28 11:18:58 -0300

    for of - estrutura de repetição

---
+++ curso da udemy/aula37/main.js
@@ -0,0 +1,28 @@
+const nome = ['Emerson', 'Thiago', 'João'];
+
+
+// For clássico - Geralmente com iteráveis (arrays ou string)
+// For in - Retorna o índice ou chave (string, array ou objeto)
+// For of - Retorna o valor em si (iteráveis, arrays ou string)
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);
+}
+
+console.log('#####')
+
+for (let i in nome) {
+    console.log(nome[i]);
+}
+
+console.log('#####')
+
+for (let valor of nome) {
+    console.log(valor);
+}
+
+console.log('#####')
+
+nome.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array)
+})
