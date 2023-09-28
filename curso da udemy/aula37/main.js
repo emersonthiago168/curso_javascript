00f748d64444d1b9a8a1a57ac44475fc82586df3
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-28 12:13:46 -0300

    exercício com nodelist (browser)

--- curso da udemy/aula37/main.js
+++ curso da udemy/aula37/main.js
@@ -3,7 +3,7 @@ const nome = ['Emerson', 'Thiago', 'João'];
 
 // For clássico - Geralmente com iteráveis (arrays ou string)
 // For in - Retorna o índice ou chave (string, array ou objeto)
-// For of - Retorna o valor em si (iteráveis, arrays ou string)
+// For of - Retorna o valor em si (iteráveis, arrays ou stringcurso da udemy/modelo/assets curso da udemy/modelo/index.html)
 
 for (let i = 0; i < nome.length; i++) {
     console.log(nome[i]);
