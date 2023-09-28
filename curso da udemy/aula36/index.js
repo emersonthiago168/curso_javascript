17cd9668d622a70d4c65b56b96504d6591ea762d
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-28 10:56:19 -0300

    for in - estrutura de repetição

---
+++ curso da udemy/aula36/index.js
@@ -0,0 +1,17 @@
+const pessoa = {
+    nome: 'Pedro',
+    sobrenome: 'Henrique',
+    idade: 30
+};
+
+for (let chave in pessoa) {
+    console.log(pessoa[chave])
+}
+
+
+
+
+// for (let indice in frutas) {
+//     console.log(frutas[indice])
+// }
+
