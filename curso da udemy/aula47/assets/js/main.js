e650ff03546316c5d871344ea36e07596a77c7e5
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-29 11:08:37 -0300

    Update main.js

--- curso da udemy/aula47/assets/js/main.js
+++ curso da udemy/aula47/assets/js/main.js
@@ -1,18 +1,35 @@
+function getTimeFromSeconds (segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {hour12: false, timeZone: 'GMT'})
+}
+
 const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+let segundos = 0;
+let timer;
 
+function iniciaRelogio () {
+    timer = setInterval(function () {
+        segundos++
+        relogio.innerHTML = getTimeFromSeconds(segundos);
+    }, 1000)
+}
 
-iniciar.addEventListener('click', function(event) {
-    
+iniciar.addEventListener('click', function (event) {
+    relogio.style.color = '#000'
+    iniciaRelogio();
 })
 
-pausar.addEventListener('click', function(event) {
-    
+pausar.addEventListener('click', function (event) {
+    relogio.style.color = '#f00'
+    clearInterval(timer)
 }
 )
 
-zerar.addEventListener('click', function(event) {
-    
+zerar.addEventListener('click', function (event) {
+    relogio.style.color = '#000'
+    clearInterval(timer);
+    relogio.innerHTML = getTimeFromSeconds(0)
 })
