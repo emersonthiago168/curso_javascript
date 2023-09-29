fbfb9b298e594ae60e52d2a3b712d40491e8b432
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-29 10:12:35 -0300

    exercício - criando um timer com setInterval

---
+++ curso da udemy/aula47/assets/js/main.js
@@ -0,0 +1,18 @@
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
+
+
+iniciar.addEventListener('click', function(event) {
+    
+})
+
+pausar.addEventListener('click', function(event) {
+    
+}
+)
+
+zerar.addEventListener('click', function(event) {
+    
+})
