048a5b8fa0de2a49f4333808a44f1a029ee43728
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-03 17:02:01 -0300

    parâmetros da função

--- curso da udemy/aula50/index.js
+++ curso da udemy/aula50/index.js
@@ -1,11 +1,11 @@
-function conta(operador, acumulador, ...numeros) {
-    for(let numero of numeros) {
+const conta = function (operador, acumulador, ...numeros) {
+    for (let numero of numeros) {
         if (operador === '+') acumulador += numero;
         if (operador === '-') acumulador -= numero;
         if (operador === '/') acumulador /= numero;
         if (operador === '*') acumulador *= numero;
     }
 
-    console.log(acumulador)
-}
+    console.log(acumulador);
+};
 conta('+', 1, 20, 30, 40, 50);
