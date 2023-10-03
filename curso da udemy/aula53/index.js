ba3d0fc9a7d4a17a24a54bbbb232a10f1237c70a
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-03 18:58:17 -0300

    closures

--- curso da udemy/aula53/index.js
+++ curso da udemy/aula53/index.js
@@ -1,10 +1,10 @@
 // Escopo Global
-function retornaFuncao() {
-    const nome = 'Luiz';
+function retornaFuncao(nome) {
     return function () {
         return nome
     }
 };
 
-const funcao = retornaFuncao();
-console.dir(funcao);
+const funcao = retornaFuncao('Luiz');
+const funcao2 = retornaFuncao('João')
+console.log(funcao(), funcao2());
