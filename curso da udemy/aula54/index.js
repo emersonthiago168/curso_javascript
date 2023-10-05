bf998eb8fea9c7d36ea3a3f122378602ee78dd23
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-05 16:47:13 -0300

    callback

--- curso da udemy/aula54/index.js
+++ curso da udemy/aula54/index.js
@@ -1,13 +1,26 @@
+function rand(min = 1000, max = 3000) {
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num);
+}
+
 function f1() {
-    console.log('f1')
+    setTimeout(function () {
+        console.log('f1')
+    }, rand()); 
 }
 function f2() {
-    console.log('f2')
+    setTimeout(function () {
+        console.log('f2')
+    }, rand()); 
 }
 function f3() {
-    console.log('f3')
+    setTimeout(function () {
+        console.log('f3')
+    }, rand()); 
 }
+
 
 f1();
 f2();
 f3();
+console.log('Olá mundo!');
