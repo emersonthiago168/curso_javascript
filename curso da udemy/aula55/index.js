fdba566a44d1a3c65d2c05081b7c1cdbb5ba9b50
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-05 17:44:12 -0300

    funções imediatas (IIFE)

--- curso da udemy/aula55/index.js
+++ curso da udemy/aula55/index.js
@@ -1,6 +1,16 @@
 // IIFE -> Immediately invoked function expression
 
-(function() {
-    const nome = 'Emerson';
-    console.log(nome)
-})();
+(function(idade, peso, altura) {
+    const sobrenome = 'Pedro'
+    function criaNome(nome) {
+        return `${nome} ${sobrenome}`
+    }
+
+    function falaNome(){
+        console.log(criaNome('João'))
+    }
+
+    falaNome();
+    console.log(idade, peso, altura)
+
+})(30, 79, 1.75);
