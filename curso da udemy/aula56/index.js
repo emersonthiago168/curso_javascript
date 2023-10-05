51142997afb2b8f423b060228db20a37c716a335
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-05 18:17:35 -0300

    funções fábrica (factory functions)

--- curso da udemy/aula56/index.js
+++ curso da udemy/aula56/index.js
@@ -1,15 +1,20 @@
-function criaPessoa(nome, sobrenome, altura, peso) {
+function criaPessoa(nome, sobrenome, a, p) {
     return {
         nome,
         sobrenome,
         fala(assunto) {
             return `${nome} está ${assunto}`
         },
-        altura,
-        peso
+        altura: a,
+        peso: p,
+        imc() {
+            const indice = this.peso / (this.altura ** 2);
+            return indice.toFixed(2);
+        }
     };
 }
 
-const p1 = criaPessoa ('Emerson', 'Thiago');
-
+const p1 = criaPessoa ('Emerson', 'Thiago', 1.80, 80);
+const p2 = criaPessoa('Maria', 'Souza', 1.65, 60);
+console.log(p1.imc());
 
