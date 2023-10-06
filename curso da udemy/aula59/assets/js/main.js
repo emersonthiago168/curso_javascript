9dd52ef9021c45f1ab84cf1c9d78cc66549d4845
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-06 15:44:59 -0300

    calculadora com função construtora

--- curso da udemy/aula59/assets/js/main.js
+++ curso da udemy/aula59/assets/js/main.js
@@ -1,6 +1,54 @@
-function Calculadora() {
-    this.display = document.querySelector('.display');
-}
+(function () {
+    function Calculadora() {
+        this.display = document.querySelector('.display');
 
-const calculadora = new Calculadora();
-calculadora.inicia();
+        this.inicia = () => {
+            this.cliqueBotoes();
+            this.pressEnter();
+        }
+
+        this.addDisplay = valor => this.display.value += valor;
+        this.clearDisplay = () => this.display.value = '';
+        this.delOne = () => this.display.value = this.display.value.slice(0, -1);
+        this.realizaConta = () => {
+            let conta = this.display.value;
+
+            try {
+
+                this.conta = eval(conta);
+
+                if (!conta) {
+                    alert('Digite um valor válido');
+                    return;
+                }
+
+                this.display.value = this.conta;
+            } catch {
+                alert('Digite um valor válido')
+                return
+            }
+        }
+
+        this.cliqueBotoes = () => {
+            document.addEventListener('click', e => {
+                let el = e.target;
+
+                if (el.classList.contains('btn-num')) this.addDisplay(el.innerText);
+                if (el.classList.contains('btn-clear')) this.clearDisplay();
+                if (el.classList.contains('btn-del')) this.delOne();
+                if (el.classList.contains('btn-eq')) this.realizaConta();
+            })
+        }
+
+        this.pressEnter = () => {
+            document.addEventListener('keypress', e => {
+                e.preventDefault();
+
+                if (e.key === 'Enter') this.realizaConta();
+            })
+        }
+    }
+
+    const calculadora = new Calculadora();
+    calculadora.inicia();
+})();
