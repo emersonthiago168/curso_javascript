2d000316dd64fccaebc5c24c5d8f68959acc17a5
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-06 18:30:20 -0300

    funções geradoras

--- curso da udemy/aula61/main.js
+++ curso da udemy/aula61/main.js
@@ -33,6 +33,10 @@ function* geradora5() {
         console.log('Vim do y1');
     };
 
+    return function() {
+        console.log('Vim do return');
+    }
+
     // ...
 
     yield function () {
@@ -41,3 +45,10 @@ function* geradora5() {
 }
 
 const g5 = geradora5();
+const func1 = g5.next().value;
+const func2 = g5.next().value;
+const func3 = g5.next().value;
+
+func1();
+func2();
+func3(); // Unreachable
