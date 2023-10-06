0d8f7eeaceb3e2711e59ffb630ead52d4bcb5039
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-06 14:40:51 -0300

    funções construtoras (constructor functions)

--- curso da udemy/aula58/index.js
+++ curso da udemy/aula58/index.js
@@ -3,12 +3,19 @@
 // Construtora => Pessoa (new)
 
 function Pessoa(nome, sobrenome) {
+    // Atributos ou métodos privadas
+    const ID = 123456;
+    const metodoInterno = () => {
+
+    };
+
+    // Atributos ou métodos públicos
     this.nome = nome;
     this.sobrenome = sobrenome;
 
     this.metodo = () => {
-        console.log(`${this.nome}: sou um método.`)
-    }
+        console.log(`${this.nome}: sou um método.`);
+    };
 }
 
 const p1 = new Pessoa('Emerson', 'Thiago');
