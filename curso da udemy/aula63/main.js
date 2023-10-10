f7dc4758529b4fc1a4bf2943985f97a28f4cca31
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-10 18:44:14 -0300

    metódo splice

--- curso da udemy/aula63/main.js
+++ curso da udemy/aula63/main.js
@@ -2,9 +2,23 @@
 const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
 
 // nomes.splice(índice, delete, elem1, elem2, elem3);
+
 // pop
-const removidos = nomes.splice(0, Number.MAX_VALUE);
-console.log(nomes, removidos);
+// const removidos = nomes.splice(-1, 1);
+// console.log(nomes, removidos);
+
+// shift
+// const removidos = nomes.splice(0, 1);
+// console.log(nomes, removidos)
+
+// push
+// nomes.splice(nomes.length, 0, 'Luiz');
+// console.log(nomes);
+
+
+// unshift
+nomes.splice(0, 0, 'Emerson', 'Thiago');
+console.log(nomes);
 
 
  
