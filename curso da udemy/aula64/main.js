9512b747b6c83829af5a4c6d3302a1e8c5e0c8fa
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-10 19:19:27 -0300

    Update main.js

--- curso da udemy/aula64/main.js
+++ curso da udemy/aula64/main.js
@@ -3,6 +3,6 @@ const a2 = [4, 5, 6];
 // const a3 = a1.concat(a2, [7, 8, 9], 'Luiz');
 
 // ... rest -> ... spread
-const a3 = [...a1, ...a2];
+const a3 = [...a1, ...a2, 'Luiz', ...[7, 8, 9]];
 console.log(a3);
 
