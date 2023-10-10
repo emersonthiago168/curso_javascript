948617d0a021c317e47179af7cbf1d77d8196677
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-10 19:40:33 -0300

    filter - filtrando array

--- curso da udemy/aula65/main.js
+++ curso da udemy/aula65/main.js
@@ -3,10 +3,10 @@
 // Retorne os números maiores que 10
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-function callbackFilter (valor, indice, array) {
-    return valor > 10;
-}
+// function callbackFilter (valor, indice, array) /* posso mandar só o que for usar, como o valor. */ {
+//     return valor > 10;
+// }
 
 
-const numerosFiltrados = numeros.filter(callbackFilter);
+const numerosFiltrados = numeros.filter(valor => valor > 10);
 console.log(numerosFiltrados);
