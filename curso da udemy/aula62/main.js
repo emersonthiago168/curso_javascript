b6c1b36b7e4ed4dd2da0e182e55941186d40f9fc
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-10 16:22:06 -0300

    revisão do básico em arrays

---
+++ curso da udemy/aula62/main.js
@@ -0,0 +1,8 @@
+// Valor por refência
+//                 0         1        2
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+const novo = [...nomes];
+
+novo.pop();
+console.log(nomes)
+console.log(novo);
