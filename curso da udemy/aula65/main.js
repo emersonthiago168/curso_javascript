e161b4cfa2795ba611d455281049de112287b2e2
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-10 20:03:22 -0300

    filter - filtrando array

--- curso da udemy/aula65/main.js
+++ curso da udemy/aula65/main.js
@@ -1,4 +1,4 @@
-// Filter, map, reduce
+// Filter => Sempre retorna um array (diferente) com a mesma quantidade de elementos ou menos.
 
 // Retorne os números maiores que 10
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
@@ -9,4 +9,22 @@ const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
 
 const numerosFiltrados = numeros.filter(valor => valor > 10);
-console.log(numerosFiltrados);
+// console.log(numerosFiltrados);
+
+
+// Retorne as pessoas que tem o nome com 5 letras ou mais
+// Retorne as pessoas com mais de 50 anos
+// Retorne as pessoas cujo nome termina com a
+const pessoas = [
+    { nome: 'Luiz', idade: 62},
+    { nome: 'Maria', idade: 23},
+    { nome: 'Eduardo', idade: 55},
+    { nome: 'Letícia', idade: 19},
+    { nome: 'Rosana', idade: 32},
+    { nome: 'Wallace', idade: 47}
+];
+
+const pessoasComNomeGrande = pessoas.filter(obj => obj.nome.length >= 5);
+const 
+console.log(pessoasComNomeGrande)
+
