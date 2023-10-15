5513ea8190315656caf1fd56edce9aafdb0ad7f7
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-15 17:39:08 -0300

    Object.definePropery() e Object.defineProperties()

--- curso da udemy/aula71/main.js
+++ curso da udemy/aula71/main.js
@@ -7,10 +7,12 @@ function Produto(nome, preco, estoque) {
     Object.defineProperty(this, 'estoque', {
         enumerable: true, // mostra a chave
         value: estoque, // valor
-        writable: false, // 
-        configurable: false,
+        writable: true, // pode alterar ou não
+        configurable: false, // configurável ou não
     });
 }
 
 const p1 = new Produto('Camiseta', 20, 3);
+p1.estoque = 60000;
+delete p1.estoque;
 console.log(p1);
