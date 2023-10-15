f4bfaa2fce00adbfa46c667cad5b705fa17acbc2
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-15 17:55:15 -0300

    getters e setters

---
+++ curso da udemy/aula72/main.js
@@ -0,0 +1,16 @@
+// defineProperty => Getter e Setters
+
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // mostra a chave
+        value: estoque, // valor
+        writable: true, // pode alterar ou não
+        configurable: true, // configurável ou não
+    })
+}
+
+const p1 = new Produto('Camiseta', 20, 3);
+console.log(p1);
