3db999427b6ba2de68ee5d10b601de55d7c53fdd
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-16 20:47:51 -0300

    prototype

--- curso da udemy/aula74/main.js
+++ curso da udemy/aula74/main.js
@@ -5,9 +5,14 @@ function Pessoa(nome, sobrenome) {
     this.nomeCompleto = () => `${this.nome} ${this.sobrenome}`;
 }
 
+Pessoa.prototype.nomeCompleto = function () {
+    return `${this.nome} ${this.sobrenome}`; 
+}
+
 // instância
 const pessoa1 = new Pessoa('Emerson', 'T.'); // <= Pessoa = Função construtora
 const pessoa2 = new Pessoa('Maria', 'A.'); // <= Pessoa = Função construtora
+const data = new Date(); // <= Date = Função construtora
 
 console.dir(pessoa1);
-console.dir(pessoa2);
+console.dir(data);
