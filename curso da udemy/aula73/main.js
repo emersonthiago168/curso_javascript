64587de70d915cf9a39d0d95b4848a36ca0c1c8c
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-16 18:15:58 -0300

    métodos úteis para objetos

--- curso da udemy/aula73/main.js
+++ curso da udemy/aula73/main.js
@@ -13,9 +13,9 @@ Object.defineProperty(define uma propriedade)
 */
 
 const produto = {nome: 'Produto', preco: 1.8};
-const caneca = Object.assign({}, produto, {material: 'porcelana'});
-
-caneca.nome = 'Outro nome';
-caneca.preco = 2.5;
-console.log(produto);
-console.log(caneca);
+Object.defineProperty(produto, 'nome', {
+    writable: false,
+    configurable: false,
+    value: 'Qualquer outra coisa'
+});
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome'));
