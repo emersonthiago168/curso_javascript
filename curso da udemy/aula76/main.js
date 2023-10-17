2bfb07539d3cd7cf6a6ae5232d8df9e579196fbf
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-17 19:53:37 -0300

    herança

--- curso da udemy/aula76/main.js
+++ curso da udemy/aula76/main.js
@@ -19,6 +19,9 @@ function Camiseta(nome, preco, cor) {
 Camiseta.prototype = Object.create(Produto.prototype);
 Camiseta.prototype.constructor = Camiseta;
 
+Camiseta.prototype.aumento = function (percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
 
 const camiseta = new Camiseta('Regata', 7.5, 'Preta');
 console.log(camiseta);
