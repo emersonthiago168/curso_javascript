dcd72dbde2c5b06f3fcaa4b01e9ce1869736f7ca
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-17 19:28:30 -0300

    herança

--- curso da udemy/aula76/main.js
+++ curso da udemy/aula76/main.js
@@ -1,1 +1,19 @@
-// 
+//  Produto => aumento, desconto
+// Camiseta = Cor, caneca = material
+function Produto(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+};
+
+Produto.prototype.aumento = function (quantia) {
+    this.preco += quantia;
+};
+Produto.prototype.desconto = function (quantia) {
+    this.preco -= quantia;
+}
+
+function Camiseta(nome, preco, cor) {
+    Produto.call(this, nome, preco);
+}
+
+const camiseta = new Camiseta('Regata', 7.5, 'Preta');
