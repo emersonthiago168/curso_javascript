a6aaa4bc6f489c643c8fe949da85c070646a2fcd
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-17 17:30:12 -0300

    manipulando prototypes

--- curso da udemy/aula75/main.js
+++ curso da udemy/aula75/main.js
@@ -3,3 +3,22 @@ function Produto(nome, preco) {
     this.preco = preco;
 };
 
+Produto.prototype.desconto = function (percentual) {
+    this.preco = this.preco - (this.preco * (percentual / 100));
+};
+
+Produto.prototype.aumento = function (percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+};
+
+const p1 = new Produto('Camiseta', 50);
+
+// Literal
+const p2 = {
+    nome: 'Caneca',
+    preco: 15
+};
+
+
+
+console.log(p2);
