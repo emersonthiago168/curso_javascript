15d5c5d8d7c23f2398273e9105bbc52830726942
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-17 20:11:39 -0300

    herança

--- curso da udemy/aula76/main.js
+++ curso da udemy/aula76/main.js
@@ -3,7 +3,7 @@
 function Produto(nome, preco) {
     this.nome = nome;
     this.preco = preco;
-};
+}
 
 Produto.prototype.aumento = function (quantia) {
     this.preco += quantia;
@@ -18,10 +18,35 @@ function Camiseta(nome, preco, cor) {
 }
 Camiseta.prototype = Object.create(Produto.prototype);
 Camiseta.prototype.constructor = Camiseta;
-
 Camiseta.prototype.aumento = function (percentual) {
     this.preco = this.preco + (this.preco * (percentual / 100));
 };
 
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function () {
+            return estoque;
+        },
+        set: function (valor) {
+            if (typeof valor !== 'number') return;
+            estoque = valor;
+        },
+    });
+}
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+const produto = new Produto('Genérico', 111);
 const camiseta = new Camiseta('Regata', 7.5, 'Preta');
+const caneca = new Caneca('Xícara', 13, 'Plástico', 5);
+
+console.log(caneca);
 console.log(camiseta);
+console.log(produto);
+
+
