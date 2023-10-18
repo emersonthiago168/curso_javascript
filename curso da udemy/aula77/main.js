65f1a1ebbb9e5834e4a84242318845f6942b5143
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-18 18:13:36 -0300

    exercício - validando um cpf

--- curso da udemy/aula77/main.js
+++ curso da udemy/aula77/main.js
@@ -27,6 +27,7 @@ function ValidaCPF(cpfEnviado) {
 ValidaCPF.prototype.valida = function() {
     if (typeof this.cpfLimpo === 'undefined') return false;
     if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2);
     const digito1 = this.criaDigito(cpfParcial);
@@ -48,6 +49,11 @@ ValidaCPF.prototype.criaDigito = function(cpfParcial) {
 
     const digito = 11 - (total % 11);
     return digito > 9 ? '0' : String(digito);
+}
+
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo; 
 }
 
 const cpf = new ValidaCPF('705.484.450-52');
