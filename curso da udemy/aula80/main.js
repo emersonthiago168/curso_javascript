6212568a4388f2f470b81ef45f18c4f53a0152b7
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-19 16:30:03 -0300

    objeto map()

--- curso da udemy/aula80/main.js
+++ curso da udemy/aula80/main.js
@@ -4,3 +4,17 @@ const pessoas = [
     { id: 1, nome: 'Helena' },
 ];
 
+// const novasPessoas = {};
+// for (const pessoa of pessoas) {
+//     const { id } = pessoa;
+//     novasPessoas[id] = { ...pessoa };
+// };
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas) {
+    const { id } = pessoa;
+    novasPessoas.set(id, { ...pessoa });
+};
+
+console.log(novasPessoas);
+
