d3bee3f2a24226fcd10f4f494e206305bdbb4d2a
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-19 13:53:34 -0300

    factory functions + prototype

--- curso da udemy/aula79/main.js
+++ curso da udemy/aula79/main.js
@@ -1,9 +1,20 @@
 function criaPessoa(nome, sobrenome) {
     return {
         nome, 
-        sobrenome
+        sobrenome,
+        falar() {
+            console.log(`${this.nome} está falando.`)
+        },
+        comer() {
+            console.log(`${this.nome} está comendo.`)
+        },
+        beber() {
+            console.log(`${this.nome} está  bebendo.`)
+        }
     };
 }
 
 const p1 = criaPessoa('Emerson', 'Thiago');
-console.log(p1)
+const p2 = criaPessoa('Luiz', 'Otavio');
+console.log(p1);
+console.log(p2);
