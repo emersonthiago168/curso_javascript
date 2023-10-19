bf3753af03db7ee47c0930f64ab33cf228c40b9d
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-19 14:29:11 -0300

    factory functions + prototype

--- curso da udemy/aula79/main.js
+++ curso da udemy/aula79/main.js
@@ -1,16 +1,22 @@
-function criaPessoa(nome, sobrenome) {
-    const pessoaPrototype = {
-        falar() {
-            console.log(`${this.nome} está falando.`)
-        },
-        comer() {
-            console.log(`${this.nome} está comendo.`)
-        },
-        beber() {
-            console.log(`${this.nome} está  bebendo.`)
-        }
-    };
+const falar = {
+    falar() {
+        console.log(`${this.nome} está falando.`)
+    },
+};
 
+const comer = {
+    comer() {
+        console.log(`${this.nome} está comendo.`)
+    },
+};
+
+const beber = {
+    beber() {
+        console.log(`${this.nome} está bebendo.`)
+    },
+};
+
+function criaPessoa(nome, sobrenome) {
     return Object.create(pessoaPrototype, {
         nome: { value: nome },
         sobrenome: { value: sobrenome }
