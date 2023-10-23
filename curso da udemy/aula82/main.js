49753d8a0392345d8a90c4252d229ece73439306
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-23 14:34:34 -0300

    getters e setters

--- curso da udemy/aula82/main.js
+++ curso da udemy/aula82/main.js
@@ -7,4 +7,16 @@ class Pessoa {
     get nomeCompleto() {
         return `${this.nome} ${this.sobrenome}`;
     }
+
+    set nomeCompleto(valor) {
+        valor = valor.split(' ');
+        this.nome = valor.shift();
+        this.sobrenome = valor.join(' ');
+    }
 }
+
+const p1 = new Pessoa('Emerson', 'Thiago');
+console.log(p1.nomeCompleto);
+p1.nomeCompleto = 'Rebeca Couto';
+console.log(p1.nomeCompleto);
+
