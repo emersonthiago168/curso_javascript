847a938c155d82d40d74bbce37774a5fef7cb5f5
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-23 13:25:05 -0300

    criando classes

--- curso da udemy/aula81/main.js
+++ curso da udemy/aula81/main.js
@@ -3,6 +3,18 @@ class Pessoa {
         this.nome = nome;
         this.sobrenome = sobrenome;
     }
+
+    falar() {
+        console.log(`${this.nome} está falando`);
+    }
+
+    comer() {
+        console.log(`${this.nome} está comendo`);
+    }
+
+    beber() {
+        console.log(`${this.nome} está bebendo`);
+    }
 }
 
 const p1 = new Pessoa('Emerson', 'Thiago');
