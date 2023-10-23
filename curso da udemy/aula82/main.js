97bf696f6d566d092d037e981ccc01c3692eccb1
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-23 14:17:33 -0300

    getters e setters

--- curso da udemy/aula82/main.js
+++ curso da udemy/aula82/main.js
@@ -1,17 +1,22 @@
+const _velocidade = Symbol('velocidade');
 class Carro {
     constructor(nome) {
         this.nome = nome;
-        this.velocidade = 0;
+        this[_velocidade] = 0;
+    }
+
+    get velocidade() {
+        return this[_velocidade];
     }
 
     acelerar() {
-        if (this.velocidade >= 100) return;
-        this.velocidade++;
+        if (this[_velocidade] >= 100) return;
+        this[_velocidade]++;
     }
 
     freio() {
-        if (this.velocidade <= 0) return;
-        this.velocidade--;
+        if (this[_velocidade] <= 0) return;
+        this[_velocidade]--;
     }
 }
 
@@ -21,4 +26,6 @@ for (let i = 0; i <= 200; i++) {
     c1.acelerar(); 
 }
 
-console.log(c1);
+
+c1.velocidade = 2000;
+console.log(c1.velocidade);
