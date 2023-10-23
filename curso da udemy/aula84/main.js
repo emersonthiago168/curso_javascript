940ea77bac1b3af497de26c6e13365fe86ab5fe1
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-23 19:45:22 -0300

    métodos de instância e estáticos

--- curso da udemy/aula84/main.js
+++ curso da udemy/aula84/main.js
@@ -4,12 +4,19 @@ class ControleRemoto {
         this.volume = 0;
     }
 
+    // Método de instância - referente a instância em si
     aumentarVolume() {
         this.volume += 2;
     }
 
+    // Método de instância - referente a instância em si
     diminuirVolume() {
         this.volume -= 2;
+    }
+
+    // Método estático - referente a classe
+    static trocaPilha() {
+        console.log('Ok, vou trocar');
     }
 }
 
@@ -17,4 +24,5 @@ const controle1 = new ControleRemoto('LG');
 controle1.aumentarVolume();
 controle1.aumentarVolume();
 controle1.aumentarVolume();
+ControleRemoto.trocaPilha();
 console.log(controle1);
