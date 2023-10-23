e3bf7c9bd57e60870fe4aa230ec57296ffaa4f1b
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-23 14:25:57 -0300

    getters e setters

--- curso da udemy/aula82/main.js
+++ curso da udemy/aula82/main.js
@@ -6,12 +6,14 @@ class Carro {
     }
 
     set velocidade(valor) {
+        console.log('SETTER')
         if (typeof valor !== 'number') return;
         if (valor >= 100 || valor <= 0) return;
         this[_velocidade] = valor;
     }
 
     get velocidade() {
+        console.log('GETTER')
         return this[_velocidade];
     }
 
@@ -28,10 +30,9 @@ class Carro {
 
 const c1 = new Carro('Fusca');
 
-for (let i = 0; i <= 200; i++) {
-    c1.acelerar();
-}
+// for (let i = 0; i <= 200; i++) {
+//     c1.acelerar();
+// }
 
-
-c1.velocidade = 50;
+c1.velocidade = 99;
 console.log(c1.velocidade);
