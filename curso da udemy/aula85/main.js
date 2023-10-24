570060c3a3efa431c509eb1bf0f88e288c2818ee
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-24 17:05:04 -0300

    usando classes (validando cpf)

--- curso da udemy/aula85/main.js
+++ curso da udemy/aula85/main.js
@@ -3,12 +3,18 @@ class ValidaCPF {
     constructor(cpfEnviado) {
         Object.defineProperty(this, 'cpfLimpo', {
             writable: false,
-            enumerable: false,
+            enumerable: true,
             configurable: false,
             value: cpfEnviado.replace(/\D+/g, '')
-        })
+        });
+    }
+
+    valida() {
+        if (!this.cpfLimpo) return false;
+        
+        return 'CHEGUEI AQUI'
     }
 }
 
 const validacpf = new ValidaCPF('070.987.720-03');
-console.log(validacpf)
+console.log(validacpf.valida());
