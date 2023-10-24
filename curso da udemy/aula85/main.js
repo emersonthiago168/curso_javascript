85b531c20203bd4d6fad4173bea2a0e3cf3ce60c
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-24 17:39:44 -0300

    usando classes (validando cpf)

--- curso da udemy/aula85/main.js
+++ curso da udemy/aula85/main.js
@@ -8,7 +8,7 @@ class ValidaCPF {
             value: cpfEnviado.replace(/\D+/g, '')
         });
     }
-    
+
     isSequencia() {
         return this.cpfLimpo[0].repeat(this.cpfLimpo.length) === this.cpfLimpo;
     }
@@ -16,10 +16,16 @@ class ValidaCPF {
     geraNovoCpf() {
         const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
         const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1);
     }
 
     geraDigito(cpfSemDigitos) {
-        
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for (let StringNumerica of cpfSemDigitos) {
+            total += 
+        }
     }
 
     valida() {
