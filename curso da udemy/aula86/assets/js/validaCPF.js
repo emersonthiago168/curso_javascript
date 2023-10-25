a3ccffc2c087d824fe8691e313d4766d2ffd41b5
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-25 20:38:06 -0300

    validando formulário (usando classes)

---
+++ curso da udemy/aula86/assets/js/validaCPF.js
@@ -0,0 +1,54 @@
+// 705.484.450-52 070.987.720-03
+class ValidaCPF {
+    constructor(cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false,
+            enumerable: true,
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        });
+    }
+
+    isSequencia() {
+        return this.cpfLimpo[0].repeat(this.cpfLimpo.length) === this.cpfLimpo;
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+        const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+        this.novoCpf = cpfSemDigitos + digito1 + digito2;
+    }
+
+    static geraDigito(cpfSemDigitos) {
+        let total = 0;
+        let reverso = cpfSemDigitos.length + 1;
+
+        for (let StringNumerica of cpfSemDigitos) {
+            total += reverso * Number(StringNumerica);
+            reverso--;
+        }
+
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0';
+    }
+
+    valida() {
+        if (!this.cpfLimpo) return false;
+        if (typeof this.cpfLimpo !== 'string') return false;
+        if (this.cpfLimpo.length !== 11) return false;
+        if (this.isSequencia()) return false;
+        this.geraNovoCpf();
+
+        return this.novoCpf === this.cpfLimpo;
+    }
+}
+
+let validacpf = new ValidaCPF('070.987.720-03');
+// validacpf = new ValidaCPF('999.999.999-99');
+
+// if (validacpf.valida()) {
+//     console.log('CPF válido');
+// } else {
+//     console.log('CPF inválido');
+// }
