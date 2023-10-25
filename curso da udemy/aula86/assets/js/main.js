3ace602c0c19d207230058f81f8bb05a04ed197a
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-25 19:29:12 -0300

    validando formulário (usando classes)

---
+++ curso da udemy/aula86/assets/js/main.js
@@ -0,0 +1,25 @@
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('.formulario');
+        this.eventos();
+    }
+
+    eventos() {
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e);
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const camposValidos = this.camposSaoValidos();
+    }
+
+    camposSaoValidos() {
+        let valid = true;
+    }
+
+
+}
+
+const valida = new ValidaFormulario();
