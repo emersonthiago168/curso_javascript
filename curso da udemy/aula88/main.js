5d1b072cf25702f162314959962abfb8e3f100df
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-31 15:04:54 -0300

    métodos úteis para promises

---
+++ curso da udemy/aula88/main.js
@@ -0,0 +1,15 @@
+function rand(min, max) {
+    min *= 1000;
+    max *= 1000;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if(typeof msg !== 'string') reject(new Error('ERRO'));
+
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo);
+    });
+}
