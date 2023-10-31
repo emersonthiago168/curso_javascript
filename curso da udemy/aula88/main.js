afd54ef49ccf4625c54816d8250da2390d60a704
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-31 16:03:29 -0300

    métodos úteis para promises

--- curso da udemy/aula88/main.js
+++ curso da udemy/aula88/main.js
@@ -5,25 +5,48 @@ function rand(min, max) {
 }
 
 function esperaAi(msg, tempo) {
+
     return new Promise((resolve, reject) => {
-        if (typeof msg !== 'string') reject(new Error('ERRO'));
+
+        if (typeof msg !== 'string') {
+            reject('CAÍ NO ERRO');
+            return;
+        };
 
         setTimeout(() => {
             resolve(msg.toUpperCase() + ' - Passei na promise');
+            return;
         }, tempo);
     });
 }
 
 // Promise.all | Promise.race | Promise.resolve | Promise.reject
 
-const promises = [
-    'Primeiro valor',
-    esperaAi('Promise 1', 3000),
-    esperaAi('Promise 2', 500),
-    esperaAi('Promise 3', 1000),
-    'Outro valor'
-];
+// const promises = [
+//     esperaAi(1000, rand(1, 5)),
+//     esperaAi('Promise 1', rand(1, 5)),
+//     esperaAi('Promise 2', rand(1, 5)),
+//     esperaAi('Promise 3', rand(1, 5)),
+// ];
 
-Promise.all(promises)
-    .then(valor => console.log(valor))
-    .catch(erro => console.log(erro))
+// Promise.all(promises)
+//     .then(valor => console.log(valor))
+//     .catch(erro => console.log(erro))
+
+// Promise.race(promises)
+//     .then(valor => console.log(valor))
+//     .catch(erro => console.log(erro))
+
+function baixaPagina() {
+    const emCache = true;
+
+    if(emCache) {
+        return Promise.reject('Página em cache');
+    } else {
+        return esperaAi('Baixei a página', 3000);
+    }
+}
+
+baixaPagina()
+    .then(dadosPagina => console.log('RESOLVE: ', dadosPagina))
+    .catch( e => console.log('ERRO: ', e))
