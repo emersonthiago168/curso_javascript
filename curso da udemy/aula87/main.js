698acbbc17161ddfb337131d11f3f14fc220e092
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-10-31 14:21:58 -0300

    promises

--- curso da udemy/aula87/main.js
+++ curso da udemy/aula87/main.js
@@ -1,15 +1,15 @@
 function rand(min, max) {
     min *= 1000;
     max *= 1000;
-    return Math.floor(Math.random() * (max-min) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 function esperaAi(msg, tempo) {
-    setTimeout(() => {
-        console.log(msg)
-    }, tempo);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(msg);
+        }, tempo);
+    });
 }
 
 esperaAi('Frase 1', rand(1, 3));
-esperaAi('Frase 2', rand(1, 3));
-esperaAi('Frase 3', rand(1, 3));
