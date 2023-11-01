63e93fcd12aef09358a5ce9176447331ef0f5656
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-01 20:31:34 -0300

    XMLHttpRequest (GET) + Promises

--- curso da udemy/aula90/assets/js/main.js
+++ curso da udemy/aula90/assets/js/main.js
@@ -1,15 +1,17 @@
 const request = obj => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send();
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(obj.method, obj.url, true);
+        xhr.send();
 
-    xhr.addEventListener('load', () => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            obj.success(xhr.responseText);
-        } else {
-            obj.error(xhr.statusText)
-        }
-    });
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseText);
+            } else {
+                reject(xhr.statusText);
+            }
+        });
+    })
 };
 
 document.addEventListener('click', e => {
@@ -22,19 +24,20 @@ document.addEventListener('click', e => {
     }
 });
 
-function carregaPagina(el) {
+async function carregaPagina(el) {
     const href = el.getAttribute('href');
 
-    request({
+    const objConfig = {
         method: 'GET',
-        url: href,
-        success(response) {
-            carregaResultado(response);
-        },
-        error(errorText) {
-            console.log(errorText);
-        }
-    })
+        url: href
+    }
+
+    try {
+        const response = await request(objConfig);
+        carregaResultado(response);
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 function carregaResultado(response) {
