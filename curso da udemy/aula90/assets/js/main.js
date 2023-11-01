2f863f3b481fc8f60e76151f1fc63b13d40aae71
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-01 20:00:33 -0300

    XMLHttpRequest (GET) + Promises

--- curso da udemy/aula90/assets/js/main.js
+++ curso da udemy/aula90/assets/js/main.js
@@ -4,10 +4,25 @@ const request = obj => {
     xhr.send();
 
     xhr.addEventListener('load', () => {
-        if(xhr.status >= 200 && xhr.status < 300) {
+        if (xhr.status >= 200 && xhr.status < 300) {
             obj.success(xhr.responseText);
         } else {
             obj.error(xhr.statusText)
         }
-    })
+    });
+};
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+function carregaPagina(el) {
+    const href = el.getAttribute('href');
+
 }
