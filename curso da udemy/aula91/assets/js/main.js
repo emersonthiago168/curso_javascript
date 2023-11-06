690f64852f584441018b07f2cfd4189ee293b4c9
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-06 17:06:32 -0300

    fetch api (get)

---
+++ curso da udemy/aula91/assets/js/main.js
@@ -0,0 +1,50 @@
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
+
+//         xhr.addEventListener('load', () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
+//             } else {
+//                 reject(xhr.statusText);
+//             }
+//         });
+//     })
+// };
+
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+function carregaPagina(el) {
+    const href = el.getAttribute('href');
+
+    fetch(href)
+        .then(response => {
+            if(response.status !== 200) throw new Error('ERRO 404!');
+            return response.text();
+        })
+        .then(html => carregaResultado(html))
+        .catch(e => console.error(e));
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
+
+fetch('pag4.html')
+    .then(resposta => {
+        if (resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
+        return resposta.text();
+    })
+    .then(html => console.log(html))
+    .catch(e => console.error(e));
