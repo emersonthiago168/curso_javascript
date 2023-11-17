c7fab301d13c40329bc550c29cf6e4f4b585b75d
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-17 14:48:52 -0300

    webpack (boilerplate)

--- curso da udemy/aula94/webpack.config.js
+++ curso da udemy/aula94/webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path'); // CommonJS 
+const path = require('path'); // CommonJS
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public', 'assets', 'js'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            exclude: /node_modules/,
-            test: /\.js$/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presents: ['@babel/env']
-                }
-            }
-        }],
-    },
-    devtool: 'source-map'
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }]
+  },
+  devtool: 'source-map'
 };
