dbf97a85583a2ea217cd41bf9662c0cc55b381f6
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-23 14:13:58 -0300

    modulos (1)

---
+++ curso_da_udemy/node/app.js
@@ -0,0 +1,12 @@
+const nome = 'Emerson';
+const sobrenome = 'Thiago';
+
+const falaNome = () => {
+    console.log(nome, sobrenome);
+;}
+
+// module.exports.nome = nome;
+// module.exports.sobrenome = sobrenome;
+// module.exports.falaNome = falaNome;
+
+exports
