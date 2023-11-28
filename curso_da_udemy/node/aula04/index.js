ea28b665971a503fe739f0a1f0dd45a71b332f72
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-11-28 14:55:21 -0300

    listando arquivos com FS e Recursão mútua

---
+++ curso_da_udemy/node/aula04/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function readdir(rootDir) {
+    rootDir = rootDir || path.resolve(__dirname);
+    const files = await fs.readdir(rootDir);
+    walk(files);
+}
+
+async function walk(files) {
+    for (let file of files) {
+        const stats = await fs.stat();
+        console.log(files);
+    }
+}
+
+readdir('C:/Users/Administrator/Documents/GitHub/javascript/curso_da_udemy/');
