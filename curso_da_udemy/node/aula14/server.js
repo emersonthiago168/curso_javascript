4a640c8d903c6da782792042452bf836ede5d77a
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-12 18:32:41 -0300

    mongoDB - conexão e primeiro model

--- curso_da_udemy/node/aula14/server.js
+++ curso_da_udemy/node/aula14/server.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const connectionString = 'mongodb+srv://emersonthiago168:Emerson5125@cluster0.eatufk7.mongodb.net/BASEDEDADOS?retryWrites=true&w=majority';
 mongoose.connect(connectionString)
-    .then(() => console.log('Agora que a conexão ocorreu'));
+    .then(() => {
+        console.log('Conectei à base de dados');
+        app.emit('pronto');
+    })
+    .catch(e => console.log(e));
 
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal } = require('./src/middlewares/middleware');
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
@@ -20,7 +25,9 @@ app.set('view engine', 'ejs');
 app.use(middlewareGlobal);
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000'); // 127.0.0.1
-    console.log('Servidor executando na porta 3000');
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessar http://localhost:3000'); // 127.0.0.1
+        console.log('Servidor executando na porta 3000');
+    });
 });
