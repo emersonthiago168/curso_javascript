83673408d9ae8679cae96480e17eb74a7cd3390c
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-13 18:42:05 -0300

    helmet e CSFR

--- curso_da_udemy/node/aula17/server.js
+++ curso_da_udemy/node/aula17/server.js
@@ -1,23 +1,22 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.CONNECTIONSTRING)
     .then(() => {
         app.emit('pronto');
     })
     .catch(e => console.log(e));
-
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
-
 const routes = require('./routes');
 const path = require('path');
-const { middlewareGlobal } = require('./src/middlewares/middleware');
+const helmet = require('helmet');
+const csrf = require('csurf');
+const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
@@ -38,7 +37,10 @@ app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 // Nossos próprios Middlewares
+app.use(csrf());
 app.use(middlewareGlobal);
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
 app.on('pronto', () => {
--- curso_da_udemy/node/aula17/src/middlewares/middleware.js
+++ curso_da_udemy/node/aula17/src/middlewares/middleware.js
@@ -6,3 +6,15 @@ exports.middlewareGlobal = (req, res, next) => {
 exports.outroMiddleware = (req, res, next) => {
     next()
 }
+
+exports.checkCsrfError = (err, req, res, next) => {
+    if (err && err.code === 'EBADCSRFTOKEN') {
+        return res.render('../views/includes/404');
+    }
+}
+
+exports.csrfMiddleware = (req, res, next) => {
+    res.locals.csrfToken = req.csrfToken();
+    next();
+}
+
