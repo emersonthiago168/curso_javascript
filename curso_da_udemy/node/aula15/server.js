6c05229709c7ec0833944ba2a83cd484834050e9
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-12-13 14:00:02 -0300

    express session e flash messages

--- curso_da_udemy/node/aula15/server.js
+++ curso_da_udemy/node/aula15/server.js
@@ -10,12 +10,29 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     })
     .catch(e => console.log(e));
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+
 const routes = require('./routes');
 const path = require('path');
 const { middlewareGlobal } = require('./src/middlewares/middleware');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: 'asdasd25456564',
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+});
+app.use(sessionOptions);
+app.use(flash());
 
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
